c20ed01f44c181497ce0f21823be32dcb3a1f274
rohangromo <rohan.gromo@gromo.in>
2024-04-09 15:37:34 +0530

    ip fetch change

--- src/interceptors/ExtractIPAddress.ts
+++ src/interceptors/ExtractIPAddress.ts
@@ -1,16 +1,40 @@
 import { NetworkInterfaceInfo, networkInterfaces } from 'os';
 export class ExtractIPAddress {
-   
 
+    extractIP(networkInterfaces: { [interfaceName: string]: NetworkInterfaceInfo[] }) {
 
-      extractIP(networkInterfaces: { [interfaceName: string]: NetworkInterfaceInfo[] }) {
-        const ipAddresses: { [interfaceName: string]: string[] } = {};
+      let enInterfaces: string[] = [];
+      let ethInterfaces: string[] = [];
     
-        for (const [interfaceName, interfaceDetails] of Object.entries(networkInterfaces)) {
-          ipAddresses[interfaceName] = interfaceDetails.map(detail => detail.address);
+
+      for (const [interfaceName, _interfaceDetails] of Object.entries(networkInterfaces)) {
+        if (interfaceName.startsWith('en')) {
+          enInterfaces.push(interfaceName);
+        } else if (interfaceName.startsWith('eth')) {
+          ethInterfaces.push(interfaceName);
         }
-    
-        return ipAddresses;
       }
+    
+      // Sort interfaces by numerical suffix (if present)
+      const sortByNumericalSuffix = (a: string, b: string): number => {
+        const numA: number = parseInt(a.match(/\d+$/) ? a.match(/\d+$/)![0] : '0', 10);
+        const numB: number = parseInt(b.match(/\d+$/) ? b.match(/\d+$/)![0] : '0', 10);
+        return numA - numB;
+      };
+    
+      enInterfaces.sort(sortByNumericalSuffix);
+      ethInterfaces.sort(sortByNumericalSuffix);
+    
+      // Select the preferred interface
+      const preferredInterface: string | undefined = enInterfaces[0] || ethInterfaces[0];
+    
+      if (preferredInterface) {
+        const ipAddress = networkInterfaces[preferredInterface].map(detail => detail.address);
+        // Return the first IP address from the details of the preferred interface
+        return ipAddress;
+      } else {
+        return null; // Return null when no matching interface is found
+      }
+    }
     
 }
