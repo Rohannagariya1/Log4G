e9d998083073b806d5cd1329f4e834dbbc67078b
rohangromo <rohan.gromo@gromo.in>
2024-04-29 19:04:06 +0530

    minor change

--- src/logger/InputHandler.ts
+++ src/logger/InputHandler.ts
@@ -1,3 +1,5 @@
+import logger from './GroMoLogger';
+
 export class InputHandler { 
     processArgs(args: any[]): { message: string; context: string; id: string , error : Error | undefined } {
       let error: Error | undefined;
@@ -6,11 +8,27 @@ export class InputHandler {
               // Capture the first Error encountered and ignore others if multiple are present
               if (!error) error = arg;
           } else if (typeof arg === 'object') {
-                  acc.push(JSON.stringify(arg));
-              
+            try {
+                // Attempt to stringify the object
+                acc.push(JSON.stringify(arg));
+            } catch (e:any) {
+                if (e instanceof TypeError) {
+                    // Log the error and push a placeholder or error description
+                    logger.error("Error serializing object:", e.message);
+                    acc.push("[Serialization Error]");
+                } else {
+                    // For any other type of error, rethrow it
+                    logger.error("Error in logging this object",e.message);
+                }
+            }
           } else {
               // Convert non-objects to string directly
-              acc.push(String(arg));
+              try{
+                acc.push(String(arg));
+              }
+              catch(e:any){
+              logger.error("Error in printing object:", e.message);
+              }
           }
           return acc;
       }, []);
